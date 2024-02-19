070229cab8abe1b8d826895907c6825a77b43665
porrinha09 <142546604+porrinha09@users.noreply.github.com>
2024-02-18 21:33:23 -0300

    Create build.gradle.kts

---
+++ DeleteMessage/build.gradle.kts
@@ -0,0 +1,14 @@
+version = "1.0.0"
+description = "delete your messages easily"
+
+aliucord {
+    author("weszin3", 1138233300926857326)
+
+    changelog.set(
+            """
+            # 1.0.0
+            * Anything
+            
+            """.trimIndent()
+    )
+}
